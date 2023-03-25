32215f951e4a1ffa3ab7a0d88a149efa5c3ca68b
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-03-24 19:24:21 -0700

    Attempts alternative mondayWork function.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 saturdayFun = (activity = "roller-skate") => `This Saturday, I want to ${activity}!`;
     
-
+/*
 const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+*/
+
+function mondayWork (activity = "go to the office") {
     return `This Monday, I will ${activity}.`;
 }
 
