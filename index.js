722f610f7481bbbdb8679b04444a4746cf1e67e2
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-03-24 19:16:55 -0700

    Demonstrates various JavaScript capabilities with functions.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+saturdayFun = (activity = "roller-skate") => `This Saturday, I want to ${activity}!`;
+    
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*"){
+    return (adjective = "special") => `You are ${flair}${adjective}${flair}!`;
+}
